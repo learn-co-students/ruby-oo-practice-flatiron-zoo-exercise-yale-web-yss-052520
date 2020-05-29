74abf42b365de950d769e9eea5600797be427d33
nicolewang2000 <nicole.wang@yale.edu>
2020-05-29 17:32:38 -0400

    done

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,34 @@
 class Zoo
+    attr_accessor :name, :location
+    @@all = []
+   
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+    
+    def animal_species
+        animals.map {|animal| animal.species}.uniq 
+    end
+    
+    def find_by_species(type)
+        animals.select {|animal| animal.species == type} 
+    end
 
+    def animal_nicknames 
+        animals.map {|animal| animal.nickname}
+    end
+   
+    def self.find_by_location(location)
+        self.all.select{|zoo| zoo.location == location}
+    end
 end
