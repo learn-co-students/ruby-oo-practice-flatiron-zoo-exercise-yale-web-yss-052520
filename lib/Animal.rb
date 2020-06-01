16df8a8e92903325eedf20fe45534faabe8a402f
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-01 17:00:25 -0400

    done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,25 @@
 class Animal
 
-end
+    attr_accessor :weight, :zoo 
+    attr_reader :species, :nickname
+
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species 
+        @weight = weight 
+        @nickname = nickname 
+        @@all << self
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.find_by_species(species)
+        self.all.select { |animal| animal.species == species}
+    end 
+
+end 
+
+
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class Zoo
+
+    attr_accessor :name, :location
+    
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def animals
+        Animal.all.select do |animal| 
+            animal.zoo == self  
+        end 
+    end 
+
+    def animal_species 
+        animals.map {|animal| animal.species}.uniq
+    end 
+
+    def animal_nicknames 
+        animals.map do |animal| 
+            animal.nickname 
+        end 
+    end 
+
+    def find_by_species(species)
+        animals.select do |animal|
+            animal.species == species
+        end 
+    end 
+
+    def self.find_by_location(location)
+        self.all.select { |zoo| zoo.location == location}
+    end 
+
 
 end
