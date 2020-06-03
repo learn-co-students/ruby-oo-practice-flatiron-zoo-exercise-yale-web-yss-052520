3911ddf84aac813844daa680a0a5cabf19b23472
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-06-03 14:15:30 +0300

    Done

--- run.rb
+++ run.rb
@@ -5,6 +5,14 @@ require 'pry'
 
 #Test your code here
 
+chicagocentral = Zoo.new("ChicagoCentralZoo", "Center of Chicago")
+chicagocentral2 = Zoo.new("ChicagoCentralZoo2", "Center of Chicago")
+newyorkcentral = Zoo.new("NewYorkCentralZoo", "Center of New York")
+
+fox = Animal.new("mammal", 39, "foxy", chicagocentral)
+lion = Animal.new("mammal", 100, "simba", chicagocentral)
+bird = Animal.new("bird", 15, "baba", newyorkcentral)
+bird2 = Animal.new("bird", 20, "babaWWW", chicagocentral)
 
 binding.pry
 puts "done"
