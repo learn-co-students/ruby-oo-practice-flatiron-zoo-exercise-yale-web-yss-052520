b638905b4970905f7bc0a1fc6f139a578a622c35
davidyu7 <david.yu@yale.edu>
2020-06-04 18:19:57 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,36 @@
 class Zoo
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select{|a| a.zoo == self}
+    end
+
+    def animal_species
+        self.animals.map{|a| a.species}.uniq
+    end
+
+    def find_by_species(species)
+        self.animals.select{|a| a.species == species}
+    end
+
+    def animal_nicknames
+        self.animals.map{|a| a.nickname}
+    end
+
+    def self.find_by_location(location)
+        self.all.select{|z| z.location == location}
+    end
 
 end
